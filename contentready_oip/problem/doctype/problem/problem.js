2028817ab80733b824dca44e242018fd5d206861
Tej Pochiraju <tej@jaaga.in>
2020-02-22 18:48:27 +0530

    removed old modules

--- contentready_oip/problem/doctype/problem/problem.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, ContentReady and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Problem', {
-	// refresh: function(frm) {
-
-	// }
-});
