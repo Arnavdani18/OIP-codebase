46cf76cff02bb89901b290b4e83f1a087f823bf3
Tej Pochiraju <tej@jaaga.in>
2020-03-20 10:06:36 +0530

    added full_width option to template

--- contentready_oip/www/home/index.py
+++ contentready_oip/www/home/index.py
@@ -2,6 +2,7 @@ import frappe
 from contentready_oip import api
 
 def get_context(context):
+    context.full_width = True
     context.stats = api.get_homepage_stats()
     context = api.get_content_for_context(context, 'Problem', 'problems', limit_page_length=5)
     context = api.get_content_for_context(context, 'Solution', 'solutions', limit_page_length=5)
