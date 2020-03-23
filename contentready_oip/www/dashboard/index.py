dd9244fb2f2461ae867ff11e61c39cdf9f290a9f
Tej Pochiraju <tej@jaaga.in>
2020-03-23 15:13:55 +0530

    FIX: missing dashboard content

--- contentready_oip/www/dashboard/index.py
+++ contentready_oip/www/dashboard/index.py
@@ -11,16 +11,16 @@ def get_context(context):
         context.user_solutions = []
         context.watched_solutions = []
         context.solution_contributions = []
-        # dashboard_content = api.get_dashboard_content(limit_page_length=2)
-        # context.problems = dashboard_content['problems']
-        # context.solutions = dashboard_content['solutions']
-        # context.users = dashboard_content['users']
-        # context.user_problems = dashboard_content['user_problems']
-        # context.user_solutions = dashboard_content['user_solutions']
-        # context.watched_problems = dashboard_content['watched_problems']
-        # context.watched_solutions = dashboard_content['watched_solutions']
-        # context.problem_contributions = dashboard_content['problem_contributions']
-        # context.solution_contributions = dashboard_content['solution_contributions']
+        dashboard_content = api.get_dashboard_content(limit_page_length=2)
+        context.problems = dashboard_content['problems']
+        context.solutions = dashboard_content['solutions']
+        context.users = dashboard_content['users']
+        context.user_problems = dashboard_content['user_problems']
+        context.user_solutions = dashboard_content['user_solutions']
+        context.watched_problems = dashboard_content['watched_problems']
+        context.watched_solutions = dashboard_content['watched_solutions']
+        context.problem_contributions = dashboard_content['problem_contributions']
+        context.solution_contributions = dashboard_content['solution_contributions']
     else:
         frappe.local.flags.redirect_location = '/'
         raise frappe.Redirect
