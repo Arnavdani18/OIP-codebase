00811f13986749daeaa86e6449dcd1634ef48749
Vipin Saini <vipin@jaaga.in>
2020-04-24 13:26:44 +0530

    styling added loader for problems and solutions grid page include global css on base.html page fixed clash of enrichment modal and global modal css

--- contentready_oip/hooks.py
+++ contentready_oip/hooks.py
@@ -20,6 +20,7 @@ app_license = "MIT"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/contentready_oip/css/contentready_oip.css"
+web_include_css = ["/assets/contentready_oip/css/global.css"]
 # web_include_js = "/assets/contentready_oip/js/contentready_oip.js"
 
 # include js in page
