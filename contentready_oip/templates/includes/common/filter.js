c92f453902747cbc6eaeaf702f056ed49d4b0866
Vipin Saini <vipin@jaaga.in>
2020-05-12 10:29:50 +0530

    remove unnecessary console statements

--- contentready_oip/templates/includes/common/filter.js
+++ contentready_oip/templates/includes/common/filter.js
@@ -35,7 +35,7 @@ frappe.ready(() => {
             let sectors = localStorage.getItem('filter_sectors');
             // sectors = available_sectors.map(s => s.name);
             // sectors = JSON.stringify(sectors);
-            console.log(sectors);
+            // console.log(sectors);
             if (!sectors) {
                 // sectors = available_sectors.map(s => s.name);
                 // console.log(sectors);
@@ -58,9 +58,7 @@ frappe.ready(() => {
         let qp;
         if (Object.keys(filter_query).length) {
             const combined_query = {...existing_query, ...filter_query };
-            console.log(combined_query, filter_query);
             qp = frappe.utils.make_query_string(combined_query);
-            console.log(qp);
         }
         if (qp) {
             const curr_route = window.location.pathname;
