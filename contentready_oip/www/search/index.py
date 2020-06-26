1652d5adf798e79f35e6863d131d29b92342bfab
vipinsaini1989 <vipinsaini1989@gmail.com>
2020-06-26 20:06:15 +0530

    [Feat] Multiple words search added multiple words text search added highlight of text

--- contentready_oip/www/search/index.py
+++ contentready_oip/www/search/index.py
@@ -20,18 +20,21 @@ def get_context(context):
     # problems
     r = api.get_filtered_paginated_content(context, 'Problem', 'problems')
     context.update(r)
+    # print("\n\n\n\n", parameters)
+    context.matched_problems = api.get_searched_content('Problem', key)
     # context.matched_problems = context.problems
-    for index, p in enumerate(context.problems):
-        if p.title.lower().find(key) != -1 or p.description.lower().find(key) != -1:
-            context.matched_problems.append(p)
-            for s in p.sectors:
-                sectors.add(s.sector)
+    # for index, p in enumerate(context.problems):
+    #     if p.title.lower().find(key) != -1 or p.description.lower().find(key) != -1:
+    #         context.matched_problems.append(p)
+    #         for s in p.sectors:
+    #             sectors.add(s.sector)
     s = api.get_filtered_paginated_content(context, 'Solution', 'solutions')
     context.update(s)
-    for index, p in enumerate(context.solutions):
-        if p.title.lower().find(key) != -1 or p.description.lower().find(key) != -1:
-            context.matched_solutions.append(p)
-            for s in p.sectors:
-                sectors.add(s.sector)
+    context.matched_solutions = api.get_searched_content('Solution', key)
+    # for index, p in enumerate(context.solutions):
+    #     if p.title.lower().find(key) != -1 or p.description.lower().find(key) != -1:
+    #         context.matched_solutions.append(p)
+    #         for s in p.sectors:
+    #             sectors.add(s.sector)
     context.matched_contributors = api.get_content_recommended_for_user('User Profile', sectors)
     # print('\n\n\n', len(context.matched_problems), len(context.matched_solutions), '\n\n\n')
