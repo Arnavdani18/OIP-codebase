469689e32b6a9058b52e4d5efff8baba4e4308fd
vipinsaini1989 <vipinsaini1989@gmail.com>
2021-01-27 12:25:20 +0530

    Created multi-select dropdown for beneficiaries pre-populating db values

--- contentready_oip/contentready_oip/web_form/add_problem/add_problem.js
+++ contentready_oip/contentready_oip/web_form/add_problem/add_problem.js
@@ -33,7 +33,7 @@ frappe.ready(async () => {
   }
 
   addSdgOptions = () => {
-    const sdg_select = $('[data-fieldname="sustainable_development_goal"][data-doctype="Problem"]');
+    const sdg_select = $('select[data-fieldname="sustainable_development_goal"]');
     // add multiple attr 
     sdg_select.attr('multiple',true);
 
@@ -55,16 +55,33 @@ frappe.ready(async () => {
     });
   }
 
-  addBeneficiaryOptions = (sectors)=>{
-    console.log('sectors', sectors);
+  beneficiaryAsMultiSelect = () => {
     const beneficiary_select = $('select[data-fieldname="beneficiaries"]');
+    beneficiary_select.attr('multiple',true);
+    beneficiary_select.next().hide();
+    beneficiary_select.select2();
+  }
+  
+
+  addBeneficiaryOptions = (sectors)=>{
+    const beneficiary_select = $('select[data-fieldname="beneficiaries"]');
+
     frappe.call({
       method: 'contentready_oip.api.get_beneficiaries_from_sectors',
       args: {sectors},
       callback: function (r) {
         const message = r.message;
-        const sorted_beneficiaries = message.map(b => ({label: b, value: b}) ).sort(sortAlphabetically);
+        // adding already selected values to options
+        const existing_beneficiaries = frappe.web_form.doc.beneficiaries ?? [];
+        const beneficiary_values = existing_beneficiaries.map(val => val.beneficiary);
+        const optionsSet = new Set([...message,...beneficiary_values]);
+
+        const sorted_beneficiaries = [...optionsSet].map(b => ({label: b, value: b}) ).sort(sortAlphabetically);
         frappe.web_form.set_df_property('beneficiaries', 'options', sorted_beneficiaries);
+        
+        // re-iterating based on options available
+        // console.log('object', optionsSet);
+        beneficiary_select.val(beneficiary_values);
       }
     })
 
@@ -611,6 +628,7 @@ frappe.ready(async () => {
   pageHeadingSection();
   appendAttachLink();
   hideAttachmentsSection();
+  beneficiaryAsMultiSelect();
   addAsterisk(['title','city','description'])
 
 
