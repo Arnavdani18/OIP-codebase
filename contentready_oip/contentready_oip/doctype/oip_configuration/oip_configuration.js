79169a8d661f26f4148624476b2a621197d43106
Tej Pochiraju <tej@iotready.co>
2021-02-28 09:37:21 +0530

    Added API and config for explicit content detection

---
+++ contentready_oip/contentready_oip/doctype/oip_configuration/oip_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, ContentReady and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('OIP Configuration', {
+	// refresh: function(frm) {
+
+	// }
+});
