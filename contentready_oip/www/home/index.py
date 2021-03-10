bd3ec9b03582c766e29edf6578d6350bdb489eb9
Tej Pochiraju <tej@iotready.co>
2021-03-10 18:33:40 +0530

    Home page now uses whoosh too

--- contentready_oip/www/home/index.py
+++ contentready_oip/www/home/index.py
@@ -1,12 +1,21 @@
 import frappe
-from contentready_oip import api
+from contentready_oip import api, problem_search, solution_search
+import json
+
+RESULTS_PER_PAGE = 5
 
 def get_context(context):
     api.create_user_profile_if_missing(None,None,frappe.session.user)
     context.full_width = True
     context.stats = api.get_homepage_stats()
-    p = api.get_filtered_paginated_content(context, 'Problem', 'problems', limit_page_length=5)
-    context.update(p)
-    s = api.get_filtered_paginated_content(context, 'Solution', 'solutions', limit_page_length=5)
-    context.update(s)
-    # print(context)
+    parameters = frappe.form_dict
+    scope = {}
+    scope['sectors'] = json.loads(parameters['sectors']) if "sectors" in parameters else []
+    scope['sdg'] = json.loads(parameters['sdgs']) if parameters.get("sdgs") else []
+    scope['beneficiaries'] = json.loads(parameters['beneficiaries']) if parameters.get("beneficiaries") else []
+    matched = problem_search.search_index('*', scope=scope, limit=RESULTS_PER_PAGE)
+    context.problems = [frappe.get_doc('Problem', d['name']) for d in matched]
+    matched = solution_search.search_index('*', scope=scope, limit=RESULTS_PER_PAGE)
+    context.solutions = [frappe.get_doc('Solution', d['name']) for d in matched]
+    return context
+    
