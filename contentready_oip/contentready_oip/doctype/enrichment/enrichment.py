64b2bfb328f8b2a5751a7316b2ee73e7ae790b4b
Tej Pochiraju <tej@iotready.co>
2021-03-26 15:11:25 +0530

    Fix: Prevent crash if notification creation fails

--- contentready_oip/contentready_oip/doctype/enrichment/enrichment.py
+++ contentready_oip/contentready_oip/doctype/enrichment/enrichment.py
@@ -21,21 +21,24 @@ class Enrichment(Document):
 			self.parent_name = self.problem
 		old = self.get_doc_before_save()
 		if not old.is_published and self.is_published:
-			self.create_insert_notifications()
+			self.maybe_create_insert_notifications()
 	
-	def create_insert_notifications(self):
-		source_full_name = frappe.db.get_value('User Profile', self.owner, 'full_name')
-		content_title, content_route = frappe.db.get_value(self.parent_doctype, self.parent_name, ['title', 'route'])
-		recipient = frappe.db.get_value(self.parent_doctype, self.parent_name, 'owner')
-		notification = frappe.get_doc({
-			'doctype': 'OIP Notification',
-			'source_user': self.owner,
-			'target_user': recipient,
-			'parent_doctype': self.parent_doctype,
-			'parent_name': self.parent_name,
-			'child_doctype': self.doctype,
-			'child_name': self.name,
-			'text': '{} enriched your {}: {}'.format(source_full_name, self.parent_doctype.lower(), content_title),
-			'route': content_route + '#' + self.doctype.lower() + 's',
-		})
-		notification.save()
+	def maybe_create_insert_notifications(self):
+		try:
+			source_full_name = frappe.db.get_value('User Profile', self.owner, 'full_name')
+			content_title, content_route = frappe.db.get_value(self.parent_doctype, self.parent_name, ['title', 'route'])
+			recipient = frappe.db.get_value(self.parent_doctype, self.parent_name, 'owner')
+			notification = frappe.get_doc({
+				'doctype': 'OIP Notification',
+				'source_user': self.owner,
+				'target_user': recipient,
+				'parent_doctype': self.parent_doctype,
+				'parent_name': self.parent_name,
+				'child_doctype': self.doctype,
+				'child_name': self.name,
+				'text': '{} enriched your {}: {}'.format(source_full_name, self.parent_doctype.lower(), content_title),
+				'route': content_route + '#' + self.doctype.lower() + 's',
+			})
+			notification.save()
+		except:
+			pass
