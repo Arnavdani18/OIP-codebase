2045408ca952c639dce05532f3a260efe5d5e2d2
Tej Pochiraju <tej@iotready.co>
2022-01-02 15:38:19 +0530

    Added resource hub

---
+++ contentready_oip/contentready_oip/doctype/oip_resource/oip_resource.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, ContentReady and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('OIP Resource', {
+	// refresh: function(frm) {
+
+	// }
+});
